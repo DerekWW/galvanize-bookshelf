f87fee7198d0f14432c8af23de9abc5d69c08b8b
DerekWW <derek.wierson@gmail.com>
2016-10-04 10:11:46 -0700

    fix seed file

--- migrations/20161003151533_books.js
+++ migrations/20161003151533_books.js
@@ -3,16 +3,15 @@
 exports.up = function(knex) {
   return knex.schema.createTable('books', (table) => {
     table.increments();
-    table.varchar('title').notNullable().defaultTo('');
-    table.varchar('author').notNullable().defaultTo('');
-    table.varchar('genre').notNullable().defaultTo('');
+    table.string('title').notNullable().defaultTo('');
+    table.string('author').notNullable().defaultTo('');
+    table.string('genre').notNullable().defaultTo('');
     table.text('description').notNullable().defaultTo('');
     table.text('cover_url').notNullable().defaultTo('');
     table.timestamps(true, true);
-
   });
 };
 
 exports.down = function(knex) {
-   return knex.schema.dropTable('books');
+  return knex.schema.dropTable('books');
 };
